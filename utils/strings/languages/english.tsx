d6190e6f43dc9283b7623c485cb3ce9f0e08bfb0
Agustín Orlando <agusavior@gmail.com>
2021-02-05 00:01:09 -0300

    Agna help translation part two

--- utils/strings/languages/english.tsx
+++ utils/strings/languages/english.tsx
@@ -23,7 +23,7 @@ programming: 'Programming',
 contact_me: 'Contact me',
 contact_me_description: 'Send me an email at agusavior@gmail.com, or you may use the following form:',
 contact_me_body_placeholder: 'I need your help...',
-your_contact: 'Your e-mail',
+your_contact: 'Your E-Mail',
 some_optional_message: 'Optional message',
 send: 'Send',
 
@@ -36,37 +36,37 @@ current: 'Current',
 prototype: 'Prototype',
 personal: 'Personal',
 
-project_tmob_title: 'Taaxii - Mobile app for transportation',
+project_tmob_title: 'Taaxii - Mobile App for Transportation',
 project_tmob_body: 'The job consisted in refactoring an application. It mainly consists of maintenance tasks now. The app owner is an Argentinian company called JCor. Today the app can be found on Google Play but its use is restricted to a few Argentinian clients. Its Google Play rating is low because of its old version.',
 
-project_tgeo_title: 'Tgeo - Mobile app for drivers',
+project_tgeo_title: 'Tgeo - Mobile App for Drivers',
 project_tgeo_body: 'The job consists in maintaining and adding features to a mobile app used by drivers (of the Taaxii app) to match with people.',
 
-project_curriculumweb5_title: 'This website - My web resume',
+project_curriculumweb5_title: 'This website - My Web Resume',
 project_curriculumweb5_body: 'This web application you are using. It is hosted on an Amazon server, and it uses a custom domain.',
 
-project_matitest_title: 'Small Node.js back end',
+project_matitest_title: 'Small Node.js Back End',
 project_matitest_body: 'Small back end for testing an encrypted authentication, capable of managing entities in a database, like users and tasks.',
 
 project_kocoin_crawler_title: 'KuCoin Crawler',
 project_kocoin_crawler_body: 'Web crawler that extracts information from a website and stores it inside a Google Sheet. It was developed for the cryptocurrency company KuCoin.',
 
-project_traingular_arbitraje_title: 'Cryptocurrency triangular arbitrage',
-project_traingular_arbitraje_body: 'A bot that looks for triangular arbitrage opportunities. It was connected to an the Coin Bene exchange. The project was halted due to lack of profit.',
+project_traingular_arbitraje_title: 'Cryptocurrency Triangular Arbitrage',
+project_traingular_arbitraje_body: 'A bot that looks for triangular arbitrage opportunities. It was connected to the Coin Bene exchange. The project was halted due to lack of profits.',
 
-project_tengoque_title: 'Tengoque - Telegram bot',
+project_tengoque_title: 'Tengoque - Telegram Bot',
 project_tengoque_body: 'Telegram bot for managing reminders, using natural langage to process each of them. You can find it by searching @tengo_que_bot on Telegram.',
 
-project_ips_prototype_title: 'IPS Prototype - Mobile app for buying tubes',
+project_ips_prototype_title: 'IPS Prototype - Mobile App for Buying Tubes',
 project_ips_prototype_body: 'Mobile app prototype that was proposed to the Argentinian company IPS.',
 
 project_dragon_debate_title: 'Dragon Debate - Personal Website',
 project_dragon_debate_body: 'Website for personal use. Self-hosted. Used for storing personal \'debate audio files\'. It also had user authentication functionality which allowed creating users and signing in.',
 
-project_abochat_title: 'Abo-chat - Mobile app for chatting with lawyers',
+project_abochat_title: 'Abo-chat - Mobile App for Chatting with Lawyers',
 project_abochat_body: 'Short job that consisted in adding features to an app that allowed the user to chat with lawyers. The app is no longer available on Google Play due to legal conflicts.',
 
-project_quickvorcio_title: 'Quickvorcio - Mobile app for divorces',
+project_quickvorcio_title: 'Quickvorcio - Mobile App for Divorces',
 project_quickvorcio_body: 'Short job that consisted in adding features to an app. The app is no longer available on Google Play due to legal conflicts.',
 
 
