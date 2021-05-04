d5a3bb1e6ac87d71d778610ff09f87daffc9e276
Agustín Orlando <agusavior@gmail.com>
2021-05-04 18:30:03 -0300

    Responsive fix.

--- pages/index.tsx
+++ pages/index.tsx
@@ -4,11 +4,11 @@ import ResumeProfileSection from '@/components/ResumeProfileSection'
 
 const IndexPage = () => (
   <Layout title="Agustín's Resume">
-    <div className="text-base font-sans with-bg-gradient sm:p-4 md:p-8">
+    <div className="text-base font-sans with-bg-gradient p-2 sm:p-4 md:p-8">
 
       <div className="flex flex-row gap-4 flex-wrap sm:flex-nowrap w-full">
 
-        <ResumeProfileSection className="section flex-initial max-w-min"/>
+        <ResumeProfileSection className="section flex-initial w-full sm:max-w-min"/>
 
         <ResumeBody className="section flex-initial overflow-hidden"/>
 
