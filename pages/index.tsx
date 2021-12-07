56263ae5727f5f84bbc3eec9f00527b25476e869
Agustín Javier Orlando <agusavior@gmail.com>
2021-12-07 22:00:06 +0100

    Added justify-center in the base of the site

--- pages/index.tsx
+++ pages/index.tsx
@@ -6,14 +6,14 @@ const IndexPage = () => (
   <Layout title="Agustín's Resume">
     <div className="text-base font-sans with-bg-gradient p-2 sm:p-4 md:p-8">
 
-      <div className="flex flex-row gap-4 flex-wrap sm:flex-nowrap w-full">
+      <div className="flex flex-row gap-4 flex-wrap sm:flex-nowrap w-full justify-center">
 
         <ResumeProfileSection className="section flex-initial w-full sm:max-w-min"/>
 
         <ResumeBody className="section flex-initial overflow-hidden"/>
 
       </div>
-
+  
     </div>
   </Layout>
 )
